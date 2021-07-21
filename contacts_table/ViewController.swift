959f436a81161f362cc984fbc92572a14d11513a
petarserbo <petarserbo@gmail.com>
2021-07-21 17:31:37 +0300

    Initial Commit

---
+++ contacts_table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contacts_table
+//
+//  Created by Petar Perich on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
